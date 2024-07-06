14cd5052b671f93c40bfa76847b745f6af5c6d7e
Fabricio Dias <fabricio270909@gmail.com>
2024-07-06 14:28:00 -0300

    register and list employees

---
+++ ExerciseLists/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace ExerciseLists;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; private set; } = 0;
+
+
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Employee(int id, string name, double salary) : this(id, name)
+    {
+        Salary = SetSalary(salary);
+    }
+
+    private double SetSalary(double value)
+    {
+        return value;
+    }
+}
---
+++ ExerciseLists/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ExerciseLists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int option;
+        List<Employee> employees = new List<Employee>();
+        Employee employee;
+        
+        Console.Write("How many employees do you want to register: ");
+        int qtiEmployee = int.Parse(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("Employee register:");
+        Console.WriteLine("-----------------------------");
+
+        for (int i = 0; i < qtiEmployee; i++)
+        {
+            Console.Write("Employee id: ");
+            int employeeId = int.Parse(Console.ReadLine());
+            Console.Write("Employee name: ");
+            string employeeName = Console.ReadLine();
+
+            Console.Write("Do you want to set a employee salary?(y/n): ");
+            string salaryOption = Console.ReadLine();
+            if (salaryOption == "y" || salaryOption == "yes")
+            {
+                Console.Write("Employee salary: ");
+                double employeeSalary = double.Parse(Console.ReadLine());
+                employee = new Employee(employeeId, employeeName, employeeSalary);
+            }
+            else
+            {
+                employee = new Employee(employeeId, employeeName);
+            }
+            
+            employees.Add(employee);
+        }
+        
+        do
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1 - List employees");
+            Console.WriteLine("2 - Edit a salary");
+            Console.WriteLine("-----------------------------");
+            Console.Write("Opção: ");
+
+            option = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+
+            switch (option)
+            {
+                case 1:
+                    foreach (var employeeData in employees)
+                    {
+                        Console.WriteLine("-----------------------------");
+                        Console.WriteLine($"Id: {employeeData.Id}");
+                        Console.WriteLine($"Name: {employeeData.Name}");
+                        Console.WriteLine($"Salary: {employeeData.Salary}");
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    return;
+            }
+
+        } while (true);
+    }
+}
